84cfce62c6657f8fed78aeb62eef04ee9cc9bf4b
kumaraditya62 <kumaraditya10vaishali@gmail.com>
2025-10-17 08:21:21 +0530

    commiting changes

---
+++ a1.py
@@ -0,0 +1,5 @@
+str = "Welcome to the world of python"
+print("uppercase of string:", str.upper())
+print("lowercase of string:", str.lower())
+print("split string:", str.split())
+print("replace -", str.replace("python", "java"))
